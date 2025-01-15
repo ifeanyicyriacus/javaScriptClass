1fb5f1e98bd209f10da9727b831e2446e059cf47
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-01-15 21:13:55 +0100

    file rename

---
+++ classwork/january-2025/15-jan/exercise2.js
@@ -0,0 +1,22 @@
+let x = 2;
+let y = 4;
+
+let total = x + y;
+console.log(total);
+
+let value = 32;
+value += value % 10;
+console.log(value);
+
+let x2 = 4;
+let n = 2;
+console.log(x2 * (x2 * n));
+
+let a = 5;
+let x3 = a + 10;
+let y2 = a - 10;
+let z = a % 10;
+console.log(a / (x3 * y2 * z))
+
+
+
