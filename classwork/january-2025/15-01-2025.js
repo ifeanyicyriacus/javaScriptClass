7244d0610b50731982be525996d06a93a4e8fdec
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-01-15 14:49:20 +0100

    completed sorting of three variable

---
+++ classwork/january-2025/15-01-2025.js
@@ -0,0 +1,13 @@
+//rearrange value in variables
+let var1 = 2
+let var2 = 3
+let var3 = 4
+
+let temp = var1
+var1 = var2
+var2 = var3
+var3 = temp
+
+console.log(var1)
+console.log(var2)
+console.log(var3)
