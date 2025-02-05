eaebe8e1f3afea3136f8c42b7c0018e251914dd8
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-02-05 05:31:57 +0100

    End of class

--- multiply.test.js
+++
@@ -1,9 +0,0 @@
-const multiply = require("./multiply");
-
-test("Multiply two numbers", ()=> {
-    let num1 = 2;
-    let num2 = 4; //Arrange
-    let actual = multiply(num1, num2); //Act
-    let expected = 8;
-    expect(actual).toBe(expected);//Assert
-})
