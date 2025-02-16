b3817f0d634fdcc301522535029b5f038dcd9e5c
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-02-17 00:23:42 +0100

    2/3 pass: Working on JavaScript version of NigeriaStateGeoPoliticalZones.
    - that getGeoPoliticalZoneByState Functions Return Correct Zone Value - passed
    - that getGeoPoliticalZoneByState function throw error for wrong state - passed
    
    failed:
    """
        Error: expect(received).toEqual(expected) // deep equality
    
        Expected: "South-East"
        Received: "Abia,Anambra,Ebonyi,Enugu,Imo"
    """
    -I cannot call the toString method of the object when printing Enum

--- assignment/feb-2025/nigeria_state_and_geo_political_zone/src/enums/nigeria_state_geo_political_zones.js
+++ assignment/feb-2025/nigeria_state_and_geo_political_zone/src/enums/nigeria_state_geo_political_zones.js
@@ -5,8 +5,17 @@ const NigeriaStateGeoPoliticalZones = Object.freeze({
     NORTH_WEST : ["Kaduna", "Katisna", "Kano", "Kebbi", "Sokoto", "Jigwa", "Zamfara"],
     SOUTH_EAST : ["Abia", "Anambra", "Ebonyi", "Enugu", "Imo"],
     SOUTH_SOUTH : ["Akwa-Ibom", "Bayelsa", "Cross-River", "Delta", "Edo", "Rivers"],
-    SOUTH_WEST :["Ekiti", "Lagos", "Osun", "Ondo", "Ogun", "Oyo"]
+    SOUTH_WEST :["Ekiti", "Lagos", "Osun", "Ondo", "Ogun", "Oyo"],
 
+    __states : [],
+    constructor(states){ this.__states = states;},
+    toString(){
+        name = this.constructor.name.split("_");
+        return name[0].substring(0, 1).toUpperCase()
+            + name[0].substring(1).toLowerCase() + "-"
+            + name[1].substring(0, 1).toLowerCase()
+            + name[1].substring(1).toLowerCase();
+    }
 });
 
 module.exports = {NigeriaStateGeoPoliticalZones};
