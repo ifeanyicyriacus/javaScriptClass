b426f6b996491c97aaa3b0eaecb629aeff24f03b
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-05-05 11:19:44 +0100

    completed the counter app

---
+++ classwork/may_2025/reduxCounterApp/src/App.jsx
@@ -0,0 +1,13 @@
+import Counter from './features/counter/Counter'
+
+function App() {
+
+    return (
+        <>
+            <h1>Beautiful Counter</h1>
+            <Counter/>
+        </>
+    );
+}
+
+export default App
---
+++ classwork/may_2025/reduxCounterApp/src/features/counter/Counter.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {decrement, increment, reset} from "./counterSlice.jsx";
+
+const Counter = () => {
+
+    // const  count = useSelector(state => state.counter.value);
+    const count = useSelector(state => state.counter.value);
+    const dispatch = useDispatch()
+
+    function reduceCounter() {
+        dispatch(decrement())
+    }
+
+    function increaseCounter() {
+            dispatch(increment())
+    }
+
+    function resetCounter() {
+        dispatch(reset())
+    }
+
+    return (
+        <>
+            <h1>{count}</h1>
+            <div>
+                <button onClick={reduceCounter}>-</button>
+                <button onClick={increaseCounter}>+</button>
+                <button onClick={resetCounter}>reset</button>
+            </div>
+        </>
+    )
+}
+
+export default Counter
+
